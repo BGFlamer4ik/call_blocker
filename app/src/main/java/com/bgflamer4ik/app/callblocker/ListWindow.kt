4354eb354ce61bcb209f4cb25c168556d708c3fa
BGFlamer4ik <van.korenkowww@gmail.com>
2025-10-26 14:20:46 +0300

    History window fast add Visual improvements

--- app/src/main/java/com/bgflamer4ik/app/callblocker/ListWindow.kt
+++ app/src/main/java/com/bgflamer4ik/app/callblocker/ListWindow.kt
@@ -13,7 +13,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.requiredHeight
-import androidx.compose.foundation.layout.requiredSize
 import androidx.compose.foundation.layout.requiredWidth
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
